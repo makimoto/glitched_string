57760d5078790e5b9ae4905b7ba1b1728ef7b968
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-12-30 09:06:01 +0900

    Fix spec for ruby 1.9.3

--- spec/glitched_string_spec.rb
+++ spec/glitched_string_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require "spec_helper.rb"
 
 describe GlitchedString do
@@ -7,11 +8,11 @@ describe GlitchedString do
 
   describe "String#glitch" do
     it "generates glitched string" do
-      str.glitch.chars.size.should == str.chars.size * (8 + 8 + 8 + 1)
+      str.glitch.split(//).size.should == str.split(//).size * (8 + 8 + 8 + 1)
     end
 
     it "generates glitched string with options" do
-      str.glitch(upper: 2, middle: 4, lower: 10).chars.size.should == str.chars.size * (2 + 4 + 10 + 1)
+      str.glitch(upper: 2, middle: 4, lower: 10).split(//).size.should == str.split(//).size * (2 + 4 + 10 + 1)
     end
   end
 
@@ -19,7 +20,7 @@ describe GlitchedString do
     it "generate glitched string destructively" do
       original_str = str.clone
       str.glitch!
-      str.chars.size.should == original_str.chars.size * (8 + 8 + 8 + 1)
+      str.split(//).size.should == original_str.split(//).size * (8 + 8 + 8 + 1)
     end
   end
 end
